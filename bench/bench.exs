9069329ed2fb93bbe3533ae454613fcb9d69ff18
preciz <barna.kovacs@tutanota.com>
2024-07-23 15:08:52 +0200

    Add simple bench script that compares hash functions

---
+++ bench/bench.exs
@@ -0,0 +1,12 @@
+Mix.install([
+  {:murmur, "~> 1.0"},
+  {:benchee, "~> 1.0"}
+])
+
+string = "I'm the test string of the benchmark 50 bytes long"
+
+Benchee.run(%{
+  "hash_x86_32" => fn -> Murmur.hash_x86_32(string) end,
+  "hash_x86_128" => fn -> Murmur.hash_x86_128(string) end,
+  "hash_x64_128" => fn -> Murmur.hash_x64_128(string) end
+})
